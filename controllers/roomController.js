a06c3b996b5641b6ff3c7378b14649cfc0e5afd0
bugrayaktiyol <bugrayaktiyol@gmail.com>
2023-10-12 20:29:58 +0300

    initial commit for backend structure

---
+++ controllers/roomController.js
@@ -0,0 +1,19 @@
+const createRoom = (roomId) => {
+
+}
+
+const deleteRoom = (roomId) => {
+
+}
+
+const isRoomEmpty = (roomId) => {
+  return roomId.roomInfo[isFull];
+}
+
+const isRoomFull = (roomId) => {
+  return roomId.roomInfo[isFull];
+}
+
+const isQueueEmpty = (queueArr) => {
+  return !(queueArr.length > 0);
+}
